756a84e5b4c775b10a202e59f6deb373cfe3b164
Paul Menczel <paul@menczel.net>
2024-01-12 18:27:31 +0900

    Updated plots

--- mpl_setup.py
+++ mpl_setup.py
@@ -16,6 +16,9 @@ matplotlib.rcParams['savefig.transparent'] = True
 matplotlib.rcParams['lines.dashed_pattern'] = [3, 3]
 matplotlib.rcParams['lines.dotted_pattern'] = [1, 2]
 
+matplotlib.rcParams['axes.linewidth'] = 1
+matplotlib.rcParams['lines.linewidth'] = 1.8
+
 matplotlib.rcParams['legend.frameon'] = False
 matplotlib.rcParams['legend.borderpad'] = 0.25
 matplotlib.rcParams['legend.labelspacing'] = 0.25
@@ -24,7 +27,9 @@ matplotlib.rcParams['legend.handlelength'] = 1.65
 # https://www.nature.com/articles/nmeth.1618
 ORANGE = (230 / 255, 159 / 255, 0)
 SKY_BLUE = (86 / 255, 180 / 255, 233 / 255)
+GREEN = (0, 158 / 255, 115 / 255)
 YELLOW = (240 / 255, 228 / 255, 66 / 255)
 BLUE = (0, 114 / 255, 178 / 255)
 VERMILLION = (213 / 255, 94 / 255, 0)
+PURPLE = (204 / 255, 121 / 255, 167 / 255)
 GRAY = (0.3, 0.3, 0.3)
